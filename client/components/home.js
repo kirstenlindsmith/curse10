fffec1a7860e3d4ce5b4b6be4967d1b0f0a87e18
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-04-08 09:20:34 -0400

    bio tweaks

--- client/components/home.js
+++ client/components/home.js
@@ -13,8 +13,8 @@ const Home = () => (
 
       <p>
         I'm a graphic designer who fell in love with code. I have a natural love
-        of meticulous detail and systemizing, and a deep appreciation for
-        creating maintainable code with beautiful, intuitive UX.
+        of detail and systemizing, and a deep appreciation for creating
+        maintainable code with beautiful UX.
       </p>
 
       <p>
@@ -22,30 +22,31 @@ const Home = () => (
         combined degree in Biology, Microbiology, and Anthropology from the
         University of Massachusetts Amherst. I worked in the autism world for
         many years, as an early-intervention therapist, classroom teacher,
-        curriculum designer, and consultant. I decided to pursue my love of art
-        and design, and made a shift to graphic design, turning my consulting
-        into a side business.
+        curriculum designer, and consultant. Wanting to pursue my love of art, I
+        made a shift to graphic design, turning my consulting into a side
+        business.
       </p>
 
       <p>
-        I decided to try my hand at writing code, and the rest was history. I
-        was obsessed; I had finally found something that combined all my skills
-        and interests in one. I quit my job, and dove head-first into teaching
-        myself Javascript. I was accepted to the Grace Hopper Program at
-        Fullstack Academy of Code and there gained proficiency in full-stack app
-        development, including Node.js, React, Express, SQL/ORM databases,
-        advanced HTML & CSS, and computer science fundamentals.
+        I decided to try my hand at writing code, and the rest was history. It
+        was love at first function, and I was obsessed; I felt I had finally
+        found something that combined all my skills and interests in one. I quit
+        my job, and dove head-first into teaching myself Javascript. I was
+        accepted to the Grace Hopper Program at Fullstack Academy of Code and
+        there gained proficiency in full-stack app development, working with
+        Node.js, React, Express, SQL/ORM databases, advanced HTML & CSS, and
+        computer science fundamentals.
       </p>
 
       <p>
         Now, as a programmer, being both a graphic designer and developer allows
-        me to make sure no detail is lost in translation. When I draw up a
-        mockup of an app, I’m already planning how to build it.
+        me to make sure no detail is lost in translation. When I draw a mockup
+        of an app, I’m already planning how to build it.
       </p>
 
       <p>
-        In my spare time I enjoy both making and playing video games, drawing,
-        writing about autism, and reading all types of fantasy.
+        In my spare time I enjoy both making and playing video games, making
+        art, writing about autism, and reading all types of fantasy.
       </p>
 
       <p>
