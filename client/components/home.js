35e54e388cab594adb10d015bdf8105257c3e850
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-04-14 18:38:55 -0400

    contact page

--- client/components/home.js
+++ client/components/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => (
   <div className="container" id="home-div">
@@ -13,35 +14,19 @@ const Home = () => (
 
       <p>
         I'm a graphic designer who fell in love with code. I have a natural love
-        of detail and systemizing, and a deep appreciation for creating
-        maintainable code with beautiful UX.
+        of detail and systemizing, with an appreciation for creating
+        maintainable code, and beautiful UX.
       </p>
 
       <p>
-        I originally started out in the medical field, graduating with a
-        combined degree in Biology, Microbiology, and Anthropology from the
-        University of Massachusetts Amherst. I worked in the autism world for
-        many years, as an early-intervention therapist, classroom teacher,
-        curriculum designer, and consultant. Wanting to pursue my love of art, I
-        made a shift to graphic design, turning my consulting into a side
-        business.
+        I specialize in full-stack app development, including but not limited
+        to: Node.js, React, Redux, Express, PostgreSQL, HTML & CSS.
       </p>
 
       <p>
-        I decided to try my hand at writing code, and the rest was history. It
-        was love at first function, and I was obsessed; I felt I had finally
-        found something that combined all my skills and interests in one. I quit
-        my job, and dove head-first into teaching myself Javascript. I was
-        accepted to the Grace Hopper Program at Fullstack Academy of Code and
-        there gained proficiency in full-stack app development, working with
-        Node.js, React, Express, SQL/ORM databases, advanced HTML & CSS, and
-        computer science fundamentals.
-      </p>
-
-      <p>
-        Now, as a programmer, being both a graphic designer and developer allows
-        me to make sure no detail is lost in translation. When I draw a mockup
-        of an app, I’m already planning how to build it.
+        Being both a graphic designer and developer allows me to make sure no
+        detail is lost in translation. When I draw a mockup of an app, I’m
+        already planning how to build it.
       </p>
 
       <p>
@@ -50,10 +35,8 @@ const Home = () => (
       </p>
 
       <p>
-        Meatspace me can be found in New York City. Digital me can be found on{' '}
-        <a href="https://www.linkedin.com/in/kirstenlindsmith/">LinkedIn</a>,{' '}
-        <a href="https://github.com/kirstenlindsmith">Github</a> and{' '}
-        <a href="https://www.instagram.com/durrsten/">Instagram</a>.
+        Meatspace me can be found in New York City. Digital me can be found{' '}
+        <Link to="/contact">here</Link>.
       </p>
 
       <h3 className="right title">- Kirsten Lindsmith</h3>
