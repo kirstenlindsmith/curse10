396c2f0841bfc0f2e3ab6cb82d09f0eac050264a
kirstenlindsmith <kirstenlindsmith@gmail.com>
2019-04-14 14:41:46 -0400

    WIP hamburger

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,36 +1,46 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {elastic as Burger} from 'react-burger-menu'
 
 const Navbar = () => (
-  // <div className="navbar-fixed" id="nav">
-  <div id="nav">
-    {/* <nav className="white"> */}
-    <div className="navWrapper">
-      <div className="container">
-        <a href="/" id="logo">
-          <img id="name" src="/assets/kirstenLogo.png" />
-          <img id="definition" src="/assets/definition.png" />
-        </a>
-
-        {/* <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <a href="https://kirstenlindsmith.pixpa.com/art-portfolio">
-                Graphics
-              </a>
-            </li>
-            <li>
-              <a href="https://kirstenlindsmith.wordpress.com/">Autism Blog</a>
-            </li>
-          </ul> */}
+  <div id="entireNav">
+    <div id="nav">
+      <div className="navWrapper">
+        <div className="container">
+          <a href="/" id="logo">
+            <img id="name" src="/assets/kirstenLogo.png" />
+            <img id="definition" src="/assets/definition.png" />
+          </a>
+        </div>
       </div>
     </div>
-    {/* </nav> */}
+
+    <div id="buttonLocation" />
+
+    <div id="menuButton">
+      <span />
+      <span />
+      <span />
+    </div>
+
+    <Burger right id="burger">
+      <ul id="menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <a href="https://kirstenlindsmith.pixpa.com/art-portfolio">
+            Graphics
+          </a>
+        </li>
+        <li>
+          <a href="https://kirstenlindsmith.wordpress.com/">Autism Blog</a>
+        </li>
+      </ul>
+    </Burger>
   </div>
 )
 
